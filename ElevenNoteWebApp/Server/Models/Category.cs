6100bb04fd69660e154b7f9ff9fb787ea0454a4e
Hunter <hunterjacobibrowning@gmail.com>
2022-05-14 16:54:39 -0400

    Added Data Models in Server, and setup Connection String

---
+++ ElevenNoteWebApp/Server/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNoteWebApp.Server.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
--- ElevenNoteWebApp/Server/Startup.cs
+++ ElevenNoteWebApp/Server/Startup.cs
@@ -27,7 +27,7 @@ namespace ElevenNoteWebApp.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDatabaseDeveloperPageExceptionFilter();
